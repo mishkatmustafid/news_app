775a442da89b472d4683f02eb5e31b98c0d9ace2
mishkatmustafid <mishkatmustafid@gmail.com>
2020-06-04 15:41:10 +0600

    before deleting web dir

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,10 @@
+//ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
@@ -10,6 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('This is title'),
+      ),
       body: SafeArea(
         child:
         Text('Hello'),
---
+++ flutter_app/lib/news_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewsCard extends StatelessWidget {
+
+  final Quote quote;
+  final Function delete;
+  NewsCard({this.quote, this.delete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Center(
+              child: Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ),
+            SizedBox(height: 6,),
+            Center(
+              child: Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            SizedBox(height: 8,),
+            FlatButton.icon(
+                onPressed: delete,
+                icon: Icon(Icons.delete),
+                label: Text('Delete Quote'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
