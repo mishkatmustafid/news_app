4bdd40d679cb87d93f11007718a96031376d6ed3
mishkatmustafid <mishkatmustafid@gmail.com>
2020-06-22 14:03:37 +0600

    .

--- flutter_app/lib/news_card.dart
+++ flutter_app/lib/news_card.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'paper.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class NewsCard extends StatelessWidget {
 
   final Paper paper;
   final Function button;
   NewsCard({this.paper, this.button});
+
+  void website(String url) async{
+
+  };
 
   @override
   Widget build(BuildContext context) {
